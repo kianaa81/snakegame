68a83a70cd9042be2e66836698a701fe1f25dad5
Kiana <salmasikiana@gmail.com>
2023-08-02 18:56:59 -0600

    Add a high score to the snake game

--- main.py
+++ main.py
@@ -39,14 +39,16 @@ while game_is_on:
         or snake.head.ycor() > 280
         or snake.head.ycor() < -280
     ):
-        game_is_on = False
-        scoreboard.game_over()
+        # scoreboard.game_over()
+        scoreboard.reset()
 
     # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            # scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
